cf196a9b23fd1c0e9c983e5ee4cd3aab6cec7d8a
Allan MacKinnon <allanmac@pixel.io>
2015-11-27 11:35:12 -0800

    asserts

---
+++ assert_cuda.c
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2015 Allan MacKinnon.  All rights reserved.
+ *
+ */
+
+//
+//
+//
+
+#include <stdlib.h>
+#include <stdio.h>
+
+//
+//
+//
+
+#include "assert_cuda.h"
+
+//
+//
+//
+
+cudaError_t
+cuda_assert(const cudaError_t code, const char* const file, const int line, const bool abort)
+{
+  if (code != cudaSuccess)
+    {
+      fprintf(stderr,"cuda_assert: %s %s %d\n",cudaGetErrorString(code),file,line);
+
+      if (abort)
+        {
+          cudaDeviceReset();          
+          exit(code);
+        }
+    }
+
+  return code;
+}
+
+//
+//
+//
